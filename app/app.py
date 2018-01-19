47a0b6a7aca761fb595e7b7ac9339e090082597c
Chris Diehl <cultclassik@gmail.com>
2018-01-18 18:42:47 -0600

    Testing ssh

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 #app.py
 import falcon
 import json
+import os
 
 class r10kResource:
 
@@ -18,12 +19,13 @@ class r10kResource:
         except Exception as ex:
             raise falcon.HTTPError(falcon.HTTP_400,'Error',ex.message)
 
-        try:
-            result = json.loads(raw_json, encoding='utf-8')
-            print(json.dumps(result))
-            resp.body = json.dumps(result)
-        except ValueError:
-            raise falcon.HTTPError(falcon.HTTP_400,'Invalid JSON','Could not decode the request body. The ''JSON was incorrect.')
+        #try:
+        #    result = json.loads(raw_json, encoding='utf-8')
+        #    print(json.dumps(result))
+        #    resp.body = json.dumps(result)
+        #except ValueError:
+        #    raise falcon.HTTPError(falcon.HTTP_400,'Invalid JSON','Could not decode the request body. The ''JSON was incorrect.')
+        os.system('ssh -o StrictHostKeyChecking=no -i /key.rsa chris@puppet "sudo r10k deploy environment -pv"')
 
 api = falcon.API()
 api.add_route('/deploy', r10kResource())
